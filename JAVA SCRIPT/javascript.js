2693be0d147b9754fc9823f4f72d4d7a88b4c65b
NareshMatta <mattanareshbabu1998@gmail.com>
2024-04-13 13:11:51 +0530

    first commit

---
+++ JAVA SCRIPT/javascript.js
@@ -0,0 +1,1 @@
+Document,('dummy, type of ,the text of the taken of the scripting language of the word document it will come from javascript.js' )
---
+++ Numbers programs/export.js
@@ -0,0 +1,17 @@
+
+
+function add(x,y){
+    return x+y;
+   
+}
+
+function subtract(x,y){
+    return x-y;
+}
+function multiplecation(x,y){
+    return x*y
+}
+function devision(x,y){
+    return x/y
+}
+export {add,subtract,multiplecation,devision};
